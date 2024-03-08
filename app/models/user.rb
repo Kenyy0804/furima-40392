e33b4c427019635db1c4abfbe1ea90a74a35915c
Kenyy0804 <kenyy0804@gmail.com>
2024-03-08 17:18:16 +0900

    モデル・パスの追記とバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
          validates :family_name, presence: true
          validates :first_name, presence: true
+         end
+         with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
          validates :family_name_kana, presence: true
          validates :first_name_kana, presence: true
+         end
          validates :birth_day, presence: true
 
 end
