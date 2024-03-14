076573f9afb193c9b89c39d6d98e192da999a420
Kenyy0804 <kenyy0804@gmail.com>
2024-03-14 15:19:37 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :cost
+  belongs_to :region
+  belongs_to :shipping_day
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :cost_id, numericality: { other_than: 1 }
+  validates :region_id, numericality: { other_than: 1 }
+  validates :shipping_day_id, numericality: { other_than: 1 }
 end
