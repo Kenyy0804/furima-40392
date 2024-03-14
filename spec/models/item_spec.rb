2b4ddddf8719fb536870f6c1ab487fb4015c87af
Kenyy0804 <kenyy0804@gmail.com>
2024-03-14 18:26:35 +0900

    テストコードの記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品出品機能' do
+    context '商品出品できるとき' do
+      it 'item_nameとitem_description、item_priceとcategoryとitem_conditionとcostとregionとshipping_dayが存在すれば登録できる' do
+        
+      end
+    end
+    context '新規登録できないとき' do
+      it 'item_nameが空では登録できない' do
+      end
+      it 'item_descriptionが空では登録できない' do
+      end
+      it 'categoryが空では登録できない' do
+      end
+      it 'item_conditionが空では登録できない' do
+      end
+      it 'costが空では登録できない' do
+      end
+      it 'regionが空では登録できない' do
+      end
+      it 'shipping_dayが空では登録できない' do
+      end
+      it 'item_priceが空では登録できない' do
+      end
+      it 'item_priceは、半角数値のみ保存可能であること' do
+      end
+      it 'item_priceは、¥300~¥9,999,999の間のみ保存可能であること。' do
+      end
+      
 end
