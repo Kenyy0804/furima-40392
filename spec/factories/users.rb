4de9468bcee2fd88d02394c9e27f60aabf506c80
Kenyy0804 <kenyy0804@gmail.com>
2024-03-15 17:03:30 +0900

    rubocop2回目

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.email }
-    password              { '1a' + Faker::Internet.password(min_length: 4) }
+    password              { "1a#{Faker::Internet.password(min_length: 4)}" }
     password_confirmation { password }
     family_name           { %w[佐藤 田中 山田].sample }
     first_name            { %w[太郎 次郎 花子].sample }
