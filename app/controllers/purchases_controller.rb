496264ef03007708045f158b1d4e081cd485c951
Kenyy0804 <kenyy0804@gmail.com>
2024-03-20 02:53:28 +0900

    コントローラーのアクションの記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,11 +2,22 @@ class PurchasesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-  end
-
-  def new
+    @purchase_shipping = PurchaseShipping.new
   end
 
   def create
+    @purchase_shipping = PurchaseShipping.new(purchase_params)
+    if @purchase_shipping.valid?
+      @purchase_shipping.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_shipping).permit(:post_code, :region_id, :municipalities, :address, :building_name, :phone_number).merge(purchase_id: current_purchase.id)
   end
 end
