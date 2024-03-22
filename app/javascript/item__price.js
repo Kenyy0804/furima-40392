45c541c5e733a67594232f0b5b85eb39c64436b6
Kenyy0804 <kenyy0804@gmail.com>
2024-03-22 16:19:12 +0900

    JSの読み込みの修正

--- app/javascript/item__price.js
+++ app/javascript/item__price.js
@@ -1,4 +1,4 @@
-window.addEventListener('turbo:load', () => {
+function item_price (){
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
@@ -14,4 +14,7 @@ window.addEventListener('turbo:load', () => {
     const profitDom = document.getElementById("profit");
     profitDom.innerHTML = profit;
 })
-});
+};
+
+window.addEventListener("load", item_price);
+window.addEventListener("turbo:render", item_price);
